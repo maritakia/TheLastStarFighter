79b8fd93ba73f76d8114677abec50743ea5a786d
maritakia <maritadeery@gmail.com>
2014-04-07 17:46:57 +0100

    can i commit this please?

---
+++ Assets/Standard Assets/Particles/Sources/Textures/turnMine.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class turnMine : MonoBehaviour {
+
+
+	public float speed = 2.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,0,speed * Time.deltaTime);
+		
+	}
+}
